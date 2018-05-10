c8df2ad46c488d28950eb51f46b7006031548309
Dani Lyn <dlyn1110@gmail.com>
2018-05-10 17:57:17 +0000

    automatically backed up by learn

---
+++ lib/song.rb
@@ -0,0 +1,63 @@
+class Song
+
+  attr_accessor :name, :artist_name
+  @@all = []
+
+  def self.all
+    @@all
+  end
+
+  def save
+    self.class.all << self
+  end
+
+  def self.create
+    #class constructor
+    song = self.new
+    @@all << song
+    song
+ end
+
+ def self.new_by_name(name)
+   song = self.new
+   song.name = name
+   song
+ end
+
+ def self.create_by_name(name)
+   song = self.new_by_name(name)
+   @@all << song
+   song
+  end
+
+  def self.find_by_name(name)
+    @@all.each do |song|
+      if song.name == name
+       return song
+   end
+
+  def self.find_or_create_by_name
+    self.find_or_create_by_name(name) || self.create_by_name(name)
+  end
+
+   def self.alphabetical
+     @@all.sort_by { |song| song.name }
+   end
+
+   def self.new_from_filename(name)
+     song = self.new
+     data = name.split("-")
+
+     song.artist_name = data[0].strip
+     song.name = data[1].split(".")[0].strip
+     song
+  end
+
+  def self.create_from_filename(filename)
+    @@all << self.new_from_filename(filename)
+  end
+
+  def self.destroy_all
+    self.all.clear
+  end
+end
