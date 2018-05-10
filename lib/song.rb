6d3d2898cbc11f6920472019c61437a410fcf696
Dani Lyn <dlyn1110@gmail.com>
2018-05-10 17:59:30 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,5 +1,6 @@
-class Song
+require "pry"
 
+class Song
   attr_accessor :name, :artist_name
   @@all = []
 
@@ -12,52 +13,55 @@ class Song
   end
 
   def self.create
-    #class constructor
+    self.new.save
+    self.all.last
+  end
+
+  def self.new_by_name(name)
     song = self.new
-    @@all << song
+    song.name = name
     song
- end
+  end
 
- def self.new_by_name(name)
-   song = self.new
-   song.name = name
-   song
- end
-
- def self.create_by_name(name)
-   song = self.new_by_name(name)
-   @@all << song
-   song
+  def self.create_by_name(name)
+    song = self.create
+    song.name = name
+    song
   end
 
   def self.find_by_name(name)
-    @@all.each do |song|
-      if song.name == name
-       return song
-   end
-
-  def self.find_or_create_by_name
-    self.find_or_create_by_name(name) || self.create_by_name(name)
+    self.all.detect {|song| song.name == name}
   end
 
-   def self.alphabetical
-     @@all.sort_by { |song| song.name }
-   end
+  def self.find_or_create_by_name(name)
+    self.find_by_name(name) || self.create_by_name(name)
+  end
 
-   def self.new_from_filename(name)
-     song = self.new
-     data = name.split("-")
+  def self.alphabetical
+    self.all.sort_by {|song| song.name}
+  end
 
-     song.artist_name = data[0].strip
-     song.name = data[1].split(".")[0].strip
-     song
+  def self.parse(filename)
+    info = []
+    parsed = filename.split(" - ")
+    info << parsed[0]
+    info << parsed[1].chomp(".mp3")
+    info
+  end
+  def self.new_from_filename(filename)
+    song = self.new_by_name(self.parse(filename)[1])
+    song.artist_name = self.parse(filename)[0]
+    song
   end
 
   def self.create_from_filename(filename)
-    @@all << self.new_from_filename(filename)
+    song = self.create_by_name(self.parse(filename)[1])
+    song.artist_name = self.parse(filename)[0]
+    song
   end
 
   def self.destroy_all
     self.all.clear
   end
+
 end
